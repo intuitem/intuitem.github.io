9ade5be70df244aaebc2ba073363dbf2df39207e
Abderrahmane Smimite <smimite@gmail.com>
2023-09-23 21:53:50 +0200

    Astro v3 updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
@@ -10,10 +8,6 @@ export default defineConfig({
     tailwind(
       {applyBaseStyles: true,}
     ),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
     sitemap(),
   ],
 });
