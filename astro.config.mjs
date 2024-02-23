33187497eafeb1a10d26f85395a529241fc29601
Abderrahmane Smimite <smimite@gmail.com>
2024-02-23 22:47:31 +0100

    remove old files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,77 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { defineConfig, squooshImageService } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import icon from 'astro-icon';
+import compress from 'astro-compress';
+import tasks from './src/utils/tasks';
+import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin } from './src/utils/frontmatter.mjs';
+import { ANALYTICS, SITE } from './src/utils/config.ts';
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const whenExternalScripts = (items = []) => ANALYTICS.vendors.googleAnalytics.id && ANALYTICS.vendors.googleAnalytics.partytown ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
+import { sanityIntegration } from "@sanity/astro";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://intuitem.com",
+  site: SITE.site,
+  base: SITE.base,
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "fr"],
+    routing: {
+        prefixDefaultLocale: false
+    }
+  },
+  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
+  output: 'static',
   integrations: [
-    tailwind(
-      {applyBaseStyles: true,}
-    ),
-    sitemap(),
-  ],
+    tailwind({
+    applyBaseStyles: false
+  }), sitemap(), mdx(), icon({
+    include: {
+      tabler: ['*'],
+      'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
+    }
+  }), ...whenExternalScripts(() => partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  })), compress({
+    CSS: true,
+    HTML: {
+      'html-minifier-terser': {
+        removeAttributeQuotes: false
+      }
+    },
+    Image: false,
+    JavaScript: true,
+    SVG: false,
+    Logger: 1
+  }), tasks(), react(),
+  sanityIntegration({
+    projectId: "1c7r7wmk",
+    dataset: "production",
+    // Set useCdn to false if you're building statically.
+    useCdn: false,
+  })
+],
+  image: {
+    service: squooshImageService()
+  },
+  markdown: {
+    remarkPlugins: [readingTimeRemarkPlugin],
+    rehypePlugins: [responsiveTablesRehypePlugin]
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src')
+      }
+    }
+  }
 });
